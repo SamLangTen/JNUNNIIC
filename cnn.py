299dcad91f775549e9c2991a0a8fdc4a110943c6
SamLangTen <samlangten@outlook.com>
2019-05-24 19:10:48 +0800

    add argument

--- cnn.py
+++ cnn.py
@@ -3,22 +3,26 @@ import tempfile
 import os
 import sys
 import time
-from data_preprocessing.tfrecord import read_and_decode,get_data,clear_dir
+import getopt
+from data_preprocessing.tfrecord import read_and_decode, get_data, clear_dir
 
-def conv2d(x,W):
-    return tf.nn.conv2d(x,W,strides=[1, 1, 1, 1],padding="SAME")
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding="SAME")
+
 
 def max_pool(x):
-    return tf.nn.max_pool(x,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding="SAME")
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+
 
 def bias_variable(shape):
-    initial = tf.constant(0.0,shape=shape)
+    initial = tf.constant(0.0, shape=shape)
     return tf.Variable(initial)
+
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape=shape,stddev=0.1)
+    initial = tf.truncated_normal(shape=shape, stddev=0.1)
     return tf.Variable(initial)
-
 
 
 image_summary = tf.summary.image
@@ -28,7 +32,6 @@ merge_summary = tf.summary.merge
 SummaryWriter = tf.summary.FileWriter
 
 
-
 def cnn(x):
     keep_prob = tf.placeholder(tf.float32)
 
@@ -36,7 +39,7 @@ def cnn(x):
     with tf.name_scope("conv1"):
         W_conv1 = weight_variable([3, 3, 3, 256])
         b_conv1 = bias_variable([256])
-        h_conv1 = tf.nn.relu(conv2d(x,W_conv1) + b_conv1)
+        h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
 
     '''第一层池化'''
     with tf.name_scope("pool1"):
@@ -49,7 +52,7 @@ def cnn(x):
         W_conv2 = weight_variable([3, 3, 256, 512])
         b_conv2 = bias_variable([512])
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-        h_conv2 = tf.nn.dropout(h_conv2,keep_prob)
+        h_conv2 = tf.nn.dropout(h_conv2, keep_prob)
 
     '''第二层池化'''
     with tf.name_scope("pool2"):
@@ -85,12 +88,12 @@ def cnn(x):
     with tf.name_scope("fc1"):
         W_fc1 = weight_variable([8*8*256, 1024])
         b_fc1 = bias_variable([1024])
-        h_pool4_flat = tf.reshape(h_pool4,[-1, 8*8*256])
-        h_fc1 = tf.nn.relu(tf.matmul(h_pool4_flat,W_fc1) + b_fc1)
+        h_pool4_flat = tf.reshape(h_pool4, [-1, 8*8*256])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool4_flat, W_fc1) + b_fc1)
 
     '''dropout'''
     with tf.name_scope("dropout1"):
-        h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
     '''output: [batch_size,1024]'''
 
@@ -104,36 +107,37 @@ def cnn(x):
 
     return output, keep_prob
 
-def Loss(output,label):
+
+def Loss(output, label):
     with tf.name_scope("loss"):
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=label,logits=output)
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
+            labels=label, logits=output)
         cross_entropy_mean = tf.reduce_mean(cross_entropy)
         loss_summary = scalar_summary('loss', cross_entropy_mean)
     return cross_entropy_mean
 
-def Optimizer(output,label):
-    cross_entropy_mean = Loss(output,label)
+
+def Optimizer(output, label):
+    cross_entropy_mean = Loss(output, label)
     with tf.name_scope("Adam_Optimizer"):
         optimizer = tf.train.AdamOptimizer(0.001).minimize(cross_entropy_mean)
     return optimizer
 
-def Evaluator(output,label):
+
+def Evaluator(output, label):
     with tf.name_scope("Accuracy"):
-        correct_prediction = tf.equal(tf.argmax(label,1),tf.argmax(output,1))
-        correct_prediction = tf.cast(correct_prediction,tf.float32)
+        correct_prediction = tf.equal(
+            tf.argmax(label, 1), tf.argmax(output, 1))
+        correct_prediction = tf.cast(correct_prediction, tf.float32)
 
         accuracy = tf.reduce_mean(correct_prediction)
         acc_summary = scalar_summary('accuracy', accuracy)
     return accuracy
 
 
-
-def train():
+def train(iter_num=20, log_iter_step=10, batch_size=32):
     # 总迭代次数
-    iter_num = 20
     # 输出间隔
-    log_iter_step = 10
-    batch_size = 32
     class_num = 6
     train_tfrecord_path = './data/record/train.tfrecords'
     val_tfrecord_path = './data/record/val.tfrecords'
@@ -145,21 +149,24 @@ def train():
     x = tf.placeholder(tf.float32, [None, 128, 128, 3])
 
     label = tf.placeholder(tf.float32, [None, class_num])
-    output,keep_prob = cnn(x)
+    output, keep_prob = cnn(x)
 
     # 优化器
-    optimizer = Optimizer(output,label)
+    optimizer = Optimizer(output, label)
 
     # 评估器
-    accuracy = Evaluator(output,label)
+    accuracy = Evaluator(output, label)
 
     # 误差
-    loss = Loss(output,label)
+    loss = Loss(output, label)
 
     # 读取数据
-    train_x, train_label = get_data(train_tfrecord_path, batch_size=batch_size, one_hot=True, label_num=class_num)
-    val_x, val_label = get_data(val_tfrecord_path, batch_size=batch_size, one_hot=True, label_num=class_num)
-    test_x, test_label = get_data(test_tfrecord_path,batch_size=batch_size,one_hot=True,label_num=class_num)
+    train_x, train_label = get_data(
+        train_tfrecord_path, batch_size=batch_size, one_hot=True, label_num=class_num)
+    val_x, val_label = get_data(
+        val_tfrecord_path, batch_size=batch_size, one_hot=True, label_num=class_num)
+    test_x, test_label = get_data(
+        test_tfrecord_path, batch_size=batch_size, one_hot=True, label_num=class_num)
 
     '''保存模型'''
     saver = tf.train.Saver(max_to_keep=5)
@@ -179,27 +186,34 @@ def train():
             input_train_x, input_train_label = sess.run([train_x, train_label])
 
             # 训练一步
-            optimizer.run(session=sess, feed_dict={x:input_train_x, label:input_train_label, keep_prob:0.5})
+            optimizer.run(session=sess, feed_dict={
+                          x: input_train_x, label: input_train_label, keep_prob: 0.5})
 
-            if i%log_iter_step==0:
+            if i % log_iter_step == 0:
 
                 input_val_x, input_val_label = sess.run([val_x, val_label])
 
-                loss_value = loss.eval(session=sess, feed_dict={x:input_train_x, label:input_train_label, keep_prob:1.0})
-                train_accuracy = accuracy.eval(session=sess, feed_dict={x:input_train_x, label:input_train_label, keep_prob:1.0})
-                val_accuracy = accuracy.eval(session=sess, feed_dict={x:input_val_x, label:input_val_label, keep_prob:1.0})
-                print('===step {},loss value is {},train accuracy is {},val accuracy is {}==='.format(i,loss_value,train_accuracy,val_accuracy))
+                loss_value = loss.eval(session=sess, feed_dict={
+                                       x: input_train_x, label: input_train_label, keep_prob: 1.0})
+                train_accuracy = accuracy.eval(session=sess, feed_dict={
+                                               x: input_train_x, label: input_train_label, keep_prob: 1.0})
+                val_accuracy = accuracy.eval(session=sess, feed_dict={
+                                             x: input_val_x, label: input_val_label, keep_prob: 1.0})
+                print('===step {},loss value is {},train accuracy is {},val accuracy is {}==='.format(
+                    i, loss_value, train_accuracy, val_accuracy))
 
-                if val_accuracy>max_accuracy:
+                if val_accuracy > max_accuracy:
                     max_accuracy = val_accuracy
-                    saver.save(sess,model_path + 'model-' + str(i) + '.ckpt')
+                    saver.save(sess, model_path + 'model-' + str(i) + '.ckpt')
 
-                summary = sess.run(merge, feed_dict={x:input_train_x, label:input_train_label, keep_prob:1.0})
+                summary = sess.run(merge, feed_dict={
+                                   x: input_train_x, label: input_train_label, keep_prob: 1.0})
                 writer.add_summary(summary, i)
 
         input_test_x, input_test_label = sess.run([test_x, test_label])
 
-        test_accuracy = accuracy.eval(session=sess, feed_dict={x:input_test_x, label:input_test_label, keep_prob:1.0})
+        test_accuracy = accuracy.eval(session=sess, feed_dict={
+                                      x: input_test_x, label: input_test_label, keep_prob: 1.0})
         print('===test accuracy is {}==='.format(test_accuracy))
 
         coord.request_stop()
@@ -208,7 +222,22 @@ def train():
 
 
 if __name__ == '__main__':
-    train()
-
-
-
+    is_restore = True
+    iter_num = 10
+    log_iter = 10
+    batch_size = 32
+    try:
+        opts, args = getopt.getopt(sys.argv, 'ri:l:b:', [
+                                   'restore', 'iteration=', 'logging_iteration_step=', 'batch_size='])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-r', '--restore'):
+            is_restore = bool(arg)
+        elif opt in ('-i', '--iteration'):
+            iter_num = int(arg)
+        elif opt in ('-l', '--logging_interation_step'):
+            log_iter = int(arg)
+        elif opt in ('-b', '--batch_size'):
+            batch_size = int(arg)
+    train(iter_num, log_iter, batch_size)
